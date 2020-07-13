395dfe04879e66347cdfe8a8a47c9fff27ad02f4
Salavat Yalalov <sialalov@exadel.com>
2020-07-13 11:01:01 +0500

    init commit

---
+++ src/test/java/MyRegexTest.java
@@ -0,0 +1,17 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class MyRegexTest {
+    @Test
+    public void testMyRegexOk() {
+        assertThat("000.12.12.034".matches(new MyRegex().pattern), is(true));
+        assertThat("121.234.12.12".matches(new MyRegex().pattern), is(true));
+        assertThat("23.45.12.56".matches(new MyRegex().pattern), is(true));
+
+        assertThat("00.12.123.123123.123".matches(new MyRegex().pattern), is(false));
+        assertThat("122.23".matches(new MyRegex().pattern), is(false));
+        assertThat("Hello.IP".matches(new MyRegex().pattern), is(false));
+    }
+}
