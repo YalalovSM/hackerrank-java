f219128af23959fcacde0d1e2dd05fab1ac17f78
Salavat Yalalov <sialalov@exadel.com>
2020-10-06 15:13:28 +0500

    refactor bigdecimal solution

--- src/main/java/BigDecimalOperations.java
+++ src/main/java/BigDecimalOperations.java
@@ -1,19 +1,25 @@
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 public class BigDecimalOperations {
     public void sortInDescendingOrderAndPrint(String[] s) {
-        List<String> list = Arrays.asList(s);
-        list.sort(new Comparator<String>() {
+        Arrays.sort(s, Collections.reverseOrder(new Comparator<String>() {
+            @Override
             public int compare(String s1, String s2) {
-                return new BigDecimal(s1).compareTo(new BigDecimal(s2));
-            }
-        }.reversed());
+                if (s1 == null || s2 == null)
+                    return 0;
 
-        for (String b : list) {
-            System.out.println(b);
+                BigDecimal d1 = new BigDecimal(s1);
+                BigDecimal d2 = new BigDecimal(s2);
+
+                return d1.compareTo(d2);
+            }
+        }));
+
+        for (String str : s) {
+            System.out.println(str);
         }
     }
 }
